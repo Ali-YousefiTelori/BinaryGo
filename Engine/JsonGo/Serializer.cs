fc954818b53bb252ec178f08b33d3d7c0f42b5f2
Ali Yousefi <ali.visual.studio@gmail.com>
2019-09-07 20:41:36 +0430

    fix CurrentCulture bug to serialize json

--- Engine/JsonGo/Serializer.cs
+++ Engine/JsonGo/Serializer.cs
@@ -3,6 +3,7 @@ using JsonGo.Runtime;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
