1f472652a7dfaa94c1c8006815c9dc106e978991
Ali Yousefi <ali.visual.studio@gmail.com>
2020-06-27 18:34:58 +0430

    make buffer reader

---
+++ Engine/JsonGo/IO/BufferReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonGo.IO
+{
+    /// <summary>
+    /// fast way to read buffer from memory
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public ref struct BufferReader<T> 
+    {
+        /// <summary>
+        /// set your capacity as default size without allocate memory always
+        /// </summary>
+        /// <param name="buffer"></param>
+        public BufferReader(Span<T> buffer) : this()
+        {
+            _Buffer = buffer;
+        }
+
+        /// <summary>
+        /// position of read buffer
+        /// </summary>
+        int _position;
+
+        Span<T> _Buffer;
+
+        /// <summary>
+        /// add new bytes to buffer
+        /// </summary>
+        /// <param name="length"></param>
+        public Span<T> Read(int length)
+        {
+            var result = _Buffer.Slice(_position)
+            _position += length;
+        }
+
+        /// <summary>
+        /// get span of buffer
+        /// </summary>
+        /// <returns></returns>
+        public Span<T> ToSpan()
+        {
+            return _Buffer;
+        }
+    }
+}
