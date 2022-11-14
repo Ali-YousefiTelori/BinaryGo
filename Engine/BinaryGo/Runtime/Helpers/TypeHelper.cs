6819ae421bfb04c21b61cc50320f98c35b1ad3c5
Ali Yousefi <ali.visual.studio@gmail.com>
2022-11-14 20:08:47 +0330

    Add support for TypeHelper to get unique hash of every types #12

---
+++ Engine/BinaryGo/Runtime/Helpers/TypeHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BinaryGo.Runtime.Helpers
+{
+    /// <summary>
+    /// help you about types
+    /// </summary>
+    public class TypeHelper
+    {
+        static ConcurrentDictionary<Type, string> HashedTypes { get; set; } = new ConcurrentDictionary<Type, string>();
+        /// <summary>
+        /// Get a unique hash from a type
+        /// so if the type has any change you will see new hash
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="caculatedHash"></param>
+        /// <returns></returns>
+        public string GetTypeUniqueHash(Type type, HashSet<Type> caculatedHash = null)
+        {
+            if (HashedTypes.TryGetValue(type, out string hash))
+                return hash;
+            if (ReflectionHelper.VariableTypes.TryGetValue(type, out string variable))
+                return AddToHash(type, variable);
+            if (caculatedHash == null)
+                caculatedHash = new HashSet<Type>();
+            //skip the stackoverflow exception whn you have loop reference
+            else if (caculatedHash.Contains(type))
+                return "calculating...";
+            caculatedHash.Add(type);
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(type.FullName);
+            foreach (var property in ReflectionHelper.GetListOfProperties(type))
+            {
+                stringBuilder.Append(property.Name);
+                stringBuilder.Append(':');
+                stringBuilder.AppendLine(GetTypeUniqueHash(property.PropertyType, caculatedHash));
+            }
+            return AddToHash(type, stringBuilder.ToString());
+        }
+
+        string AddToHash(Type type, string fullName)
+        {
+            var hash = GetHash(fullName);
+            HashedTypes[type] = hash;
+            return hash;
+        }
+
+        string GetHash(string input)
+        {
+            using (var sha1 = SHA1.Create())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("X2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
---
+++ Unit Tests/BinaryGoTest/Helpers/TypeHelperTests.cs
@@ -0,0 +1,61 @@
+﻿using BinaryGo.Runtime.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace BinaryGoTest.Helpers
+{
+    public class Test1
+    {
+
+    }
+
+    public class Test2
+    {
+
+    }
+
+    public class Test3
+    {
+        public string Name { get; set; }
+        public Test1 Test1 { get; set; }
+    }
+
+    public class Test4
+    {
+        public int Age { get; set; }
+    }
+
+    public class Test5
+    {
+        public string Age { get; set; }
+    }
+
+    public class TestLoop
+    {
+        public TestLoop Age { get; set; }
+    }
+
+    public class TypeHelperTests
+    {
+        [Fact]
+        public void TestHash()
+        {
+            List<string> items = new List<string>();
+            items.Add(new TypeHelper().GetTypeUniqueHash(typeof(Test1)));
+            items.Add(new TypeHelper().GetTypeUniqueHash(typeof(Test1)));
+            items.Add(new TypeHelper().GetTypeUniqueHash(typeof(Test1)));
+            items.Add(new TypeHelper().GetTypeUniqueHash(typeof(Test2)));
+            items.Add(new TypeHelper().GetTypeUniqueHash(typeof(Test3)));
+            items.Add(new TypeHelper().GetTypeUniqueHash(typeof(Test4)));
+            items.Add(new TypeHelper().GetTypeUniqueHash(typeof(Test5)));
+            items.Add(new TypeHelper().GetTypeUniqueHash(typeof(Test5)));
+            items.Add(new TypeHelper().GetTypeUniqueHash(typeof(Test5)));
+            items.Add(new TypeHelper().GetTypeUniqueHash(typeof(TestLoop)));
+            items = items.Distinct().ToList();
+            Assert.True(items.Count == 6);
+        }
+    }
+}
